2f1dd6a974b37dc1bfc22f30db72c356898b9d4e
John Kowalski <johnkowalski.design@gmail.com>
2017-01-16 22:50:39 -0500

    added song api
    api
    text changes

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $.getJSON(
+    "http://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=kowalskiforged&api_key=19ad5193e262abca9105369798104d1a&limit=2&format=json&callback=?",
+    function(data){
+      var html = '';
+      var counter = 1;
+      $.each(data.recenttracks.track,function(i, item){
+        if(counter === 1){
+          html += 'Currently listening to:<br/> <span><a href="' + item.url + '" target="_blank">' + item.name + '</a> - ' + item.artist['#text'] + '</span>';
+        }
+        counter++;
+      });
+      $('.listening-to h5').append(html);
+  });
+});
